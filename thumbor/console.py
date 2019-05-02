c19b7433e98925c09b479396a7355b1b805228cd
Pragyan Tripathi <pntripathi9417@gmail.com>
2019-05-02 15:32:06 +0530

    fix(thumbor): fix log level for servers

--- thumbor/console.py
+++ thumbor/console.py
@@ -49,7 +49,7 @@ def get_server_parameters(arguments=None):
     )
 
     parser.add_argument(
-        "-l", "--log-level", default="debug",
+        "-l", "--log-level", default="warning",
         help="The log level to be used. Possible values are: debug, info, warning, error, critical or notset. "
         "[default: %(default)s]."
     )
